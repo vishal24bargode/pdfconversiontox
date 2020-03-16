4db776f61dcfb0e4f538be49cc3e893614e97501
vishal24bargode <vishal24bargode@gmail.com>
2020-03-16 14:13:17 +0800

    Aplication Properties
    Updated Application Properties to read HTML Output Directory

--- src/main/java/com/pdfconversiontox/util/Pdf2HtmlUtil.java
+++ src/main/java/com/pdfconversiontox/util/Pdf2HtmlUtil.java
@@ -11,6 +11,8 @@ import javax.xml.parsers.ParserConfigurationException;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.fit.pdfdom.PDFDomTree;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Repository;
 
 import com.itextpdf.text.Document;
@@ -20,17 +22,37 @@ import com.itextpdf.tool.xml.XMLWorkerHelper;
 
 @Repository
 public class Pdf2HtmlUtil {
+	
+	@Autowired
+	private Environment env;
 
 	public Boolean generateHTMLFromPDF(String pdfPath) throws ParserConfigurationException, IOException {
 		String pdfPathSplit[] = pdfPath.split("/");
 		String htmlFileName = pdfPathSplit[pdfPathSplit.length-1];
-		if(htmlFileName.contains(".pdf")) {
-			htmlFileName = htmlFileName.replace(".pdf", "");
+		
+		if(htmlFileName.endsWith(".pdf") || htmlFileName.endsWith(".PDF")) {
+			htmlFileName = htmlFileName.replace(".pdf", ".html");
+			htmlFileName = htmlFileName.replace(".PDF", ".html");
 		}
 		
+		
+		String htmlOutputDir = "src/main/resources/output/";
+		
+		if(!env.getProperty("app.pdfconversiontox.html.output.dir").isEmpty())
+		{
+			htmlOutputDir = env.getProperty("app.pdfconversiontox.html.output.dir");
+		}
+		
+		htmlFileName = htmlOutputDir+htmlFileName;
+		
+		System.out.println("[Pdf2HtmlUtil][generateHTMLFromPDF][htmlFileName]="+htmlFileName);
+		
 		PDDocument pdf = PDDocument.load(new File(pdfPath));
+
 		PDFDomTree parser = new PDFDomTree();
-		Writer output = new PrintWriter("src/main/resources/output/"+htmlFileName+".html", "utf-8");
+		
+		Writer output = new PrintWriter(htmlFileName , "UTF-16");
+		
 		parser.writeText(pdf, output);
 	
 		output.close();
